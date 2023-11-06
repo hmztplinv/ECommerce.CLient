3544132afeb43a41f89dc85671a1007848c1bbb0
hmztplinv <hmz.tpal@gmail.com>
2023-11-06 09:05:07 +0300

    valıdatıon basıc

--- ECommerceClient/src/app/admin/components/products/create-product/create-product.component.ts
+++ ECommerceClient/src/app/admin/components/products/create-product/create-product.component.ts
@@ -11,23 +11,39 @@ import { ProductService } from 'src/app/services/common/models/product.service';
   templateUrl: './create-product.component.html',
   styleUrls: ['./create-product.component.scss']
 })
-export class CreateProductComponent extends BaseComponent  implements OnInit{
+export class CreateProductComponent extends BaseComponent implements OnInit {
 
-  constructor(private productService:ProductService,spinner:NgxSpinnerService,private alertify:AlertifyService) {super(spinner) }
+  constructor(private productService: ProductService, spinner: NgxSpinnerService, private alertify: AlertifyService) { super(spinner) }
 
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
 
-  createProduct(name:HTMLInputElement,stock:HTMLInputElement,price:HTMLInputElement){
+  createProduct(name: HTMLInputElement, stock: HTMLInputElement, price: HTMLInputElement) {
     this.showSpinner(SpinnerType.ballScaleMultiple);
-    const createProduct :Product=new Product();
-    createProduct.name=name.value;
-    createProduct.stock=parseInt(stock.value);
-    createProduct.price=parseInt(price.value);
-    this.productService.createProduct(createProduct, ()=>{this.hideSpinner(SpinnerType.ballScaleMultiple);
-    this.alertify.message("Product created successfully",{type:AlertifyType.Success,position:AlertifyPosition.BottomCenter});
-    },()=>{this.hideSpinner(SpinnerType.ballScaleMultiple);});
+    const createProduct: Product = new Product();
+    createProduct.name = name.value;
+    createProduct.stock = parseInt(stock.value);
+    createProduct.price = parseInt(price.value);
+
+    if(!name.value) {
+      this.alertify.message("Product name is required",{type:AlertifyType.Error,position:AlertifyPosition.BottomLeft});
+      return;
+    }
+    if(parseInt(stock.value) <= 0) {
+      this.alertify.message("Product stock must be greater than 0",{type:AlertifyType.Error,position:AlertifyPosition.BottomLeft});
+      return;
+    }
+    if(parseInt(price.value) <= 0) {
+      this.alertify.message("Product price must be greater than 0",{type:AlertifyType.Error,position:AlertifyPosition.BottomLeft});
+      return;
+    }
+
+    this.productService.createProduct(createProduct, () => {
+      this.hideSpinner(SpinnerType.ballScaleMultiple);
+      this.alertify.message("Product created successfully", { type: AlertifyType.Success, position: AlertifyPosition.BottomCenter });
+    }, (errorMessage: string) =>  {
+      this.alertify.message(errorMessage,{type:AlertifyType.Error,position:AlertifyPosition.BottomLeft}); });
   }
 
 }
--- ECommerceClient/src/app/services/common/models/product.service.ts
+++ ECommerceClient/src/app/services/common/models/product.service.ts
@@ -1,17 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpclientService } from '../httpclient.service';
 import { Product } from 'src/app/contracts/Product';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor(private httpClientService:HttpclientService) { }
+  constructor(private httpClientService: HttpclientService) { }
 
-  createProduct(product:Product,successCallback?:any,errorCallback?:any){
-    return this.httpClientService.post({ controller: "products" }, product).subscribe((data => successCallback(data)),error=>errorCallback(error));
+  createProduct(product: Product, successCallback?: any, errorCallback?: any)  {
+     this.httpClientService.post({ controller: "products" }, product)
+      .subscribe(result => {
+        successCallback();
+      }, (errorResponse: HttpErrorResponse) => {
+        const _error: Array<{ key: string, value: Array<string> }> = errorResponse.error;
+        let message = "";
+        _error.forEach((val, index) => {
+          val.value.forEach((_val, _index) => {
+            message += `${_val}`;
+          });
+        });
+        if (errorCallback) {
+          errorCallback(message);
+        }
+      });
   }
 
-  
+
 }
